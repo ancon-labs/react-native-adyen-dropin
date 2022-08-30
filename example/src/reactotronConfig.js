fcb6b623fe300b5dbb3ac1e46281d6b8496f3f8f
Jesper Johansson <21033162+jesperjohansson@users.noreply.github.com>
2022-08-30 10:16:46 +0200

    chore(example): upgrade react-native to v67

--- example/src/reactotronConfig.js
+++ example/src/reactotronConfig.js
@@ -1,8 +1,6 @@
 import Reactotron from 'reactotron-react-native';
-import { AsyncStorage } from 'react-native';
 
-Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-  .configure() // controls connection & communication settings
+Reactotron.configure() // controls connection & communication settings
   .useReactNative({
     asyncStorage: false, // there are more options to the async storage.
     // optionally, you can turn it off with false.
